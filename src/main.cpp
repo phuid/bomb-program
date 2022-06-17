7bf75e8bbb5932862e97fbdad6ca6b1c3a76a1d2
Simon Hrouda <simon.hrouda@gmail.com>
2022-06-17 15:45:04 +0200

    standart beeping

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,28 @@ const uint8_t correct_pins[5] = {26, 27, 28, 29, 30};
 
 #define RESET_BTN_PIN 53
 const uint8_t switches[4] = {50, 51, 48, 49};                                                                        // in order 8421 (see the board)
-const uint16_t gamelenght_possibilities[16] = {0, 5, 10, 15, 20, 30, 40, 60, 80, 100, 120, 150, 180, 210, 240, 300}; // in seconds
+const uint16_t gamelenght_possibilities[16] = {5, 10, 15, 20, 30, 40, 60, 80, 100, 120, 150, 180, 210, 240, 300, 600}; // in seconds
 
 #define PIEZO_PIN 46
 #define LED_PIN LED_BUILTIN
 // led - 52
 
+#define PIEZO_SINGLEBEEP_LENGTH 100 // in ms
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 uint8_t correct_pin;
-uint16_t gamelenght;
+size_t gamelenght;
+size_t gamestart_time;
+
+bool ledvalue = false;
+bool piezovalue = false;
+
+size_t ledstart_time;
+size_t piezostart_time;
+
+size_t led_length;
+size_t piezo_length;
 
 int power(int base, int exponent)
 {
@@ -46,6 +58,27 @@ uint8_t readswitches()
     arr[i] = digitalRead(switches[i]);
   }
   return binaryToDecimal(arr);
+}
+
+void led(bool value, size_t length)
+{
+  ledvalue = 1;
+  ledstart_time = millis();
+  led_length = length;
+}
+
+void led(size_t length)
+{
+  ledvalue = !ledvalue;
+  ledstart_time = millis();
+  led_length = length;
+}
+
+void piezo(bool value, size_t length)
+{
+  piezovalue = 1;
+  piezostart_time = millis();
+  piezo_length = length;
 }
 
 void setup()
@@ -82,6 +115,10 @@ void setup()
         correct_pin = correct_pins[switches_state];
         correct_pin_selected = true;
       }
+      else {
+        Serial.print("Invalid switch state: ");
+        Serial.println(switches_state);
+      }
     }
     previous = current;
     if (current)
@@ -103,8 +140,12 @@ void setup()
       uint8_t switches_state = readswitches();
       if (switches_state < sizeof(gamelenght_possibilities) / sizeof(gamelenght_possibilities[0]))
       {
-        gamelenght = gamelenght_possibilities[switches_state];
+        gamelenght = gamelenght_possibilities[switches_state] * 1000;
         timer_selected = true;
+      }
+      else {
+        Serial.print("Invalid switch state: ");
+        Serial.println(switches_state);
       }
     }
     previous = current;
@@ -116,6 +157,39 @@ void setup()
   Serial.println(gamelenght);
 
   // wait for button -> start the game
+
+  gamestart_time = millis();
+}
+
+size_t calc_delay()
+{
+// from https://github.com/WouterGritter/CSGO-Bomb/blob/master/armed_state.ino
+/**
+ * Time between beeps is an exponential function
+ *
+ * A0 = 1.04865151217746
+ * A1 = 0.244017811416199
+ * A2 = 1.76379778668885
+ *
+ * Y(bps) = A0 * E^(A1*X + A2*X^2)
+ * X = % OF TIME PASSED (0.0 - 1.0)
+ * Y = BEEPS PER SECOND
+ */
+#define BEEP_A0 1.04865151217746
+#define BEEP_A1 0.244017811416199
+#define BEEP_A2 1.76379778668885
+  // Calculate next beep time
+  float x = (float)(millis() - gamestart_time) / gamelenght;
+  float n = BEEP_A1 * x + BEEP_A2 * x * x;
+  float bps = BEEP_A0 * exp(n);
+
+  // Convert bps (beeps per second) to a wait time in milliseconds
+  Serial.print(millis() - gamestart_time);
+  Serial.print(" ");
+  Serial.print(gamelenght);
+  Serial.print("wait: ");
+  Serial.println(1000.0 / bps);
+  return 1000.0 / bps;
 }
 
 void loop()
@@ -126,5 +200,17 @@ void loop()
 
   // check timer
 
+  // set the led and piezo variables
+  ////normal ticking
+  if (millis() - ledstart_time > led_length)
+  {
+    piezo(1, PIEZO_SINGLEBEEP_LENGTH);
+    led(calc_delay());
+  }
+
+  ////beep sequences
+
   // set the led and piezo
+  digitalWrite(LED_PIN, ledvalue);
+  digitalWrite(PIEZO_PIN, piezovalue);
 }
